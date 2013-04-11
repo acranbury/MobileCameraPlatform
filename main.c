825b0f742df05ebcdac5d397be4d6db3e5ce0f10
Jack Morgan <jack@ehelion.com>
2013-04-11 12:57:29 -0400

    Main source and SCI module

---
+++ main.c
@@ -0,0 +1,150 @@
+
+#include <hidef.h>      /* common defines and macros */
+#include <string.h>
+#include "derivative.h"      /* derivative-specific definitions */
+#include "timer.h"
+#include "sci.h"
+#include "lcd.h"
+
+
+// command size
+#define CMD_LEN 3
+#define PNG 1
+#define ABT 2
+#define TXT 3
+#define PAN 4
+#define TLT 5
+
+void cmdparser (char * buffer);
+int cmdconv (char * tempcmd) ;
+void seekcmd (unsigned char * buffer, int * numchars) ;
+
+
+#pragma MESSAGE DISABLE C1420   // Function call result ignored warning disable (for memset)
+void main(void) {
+    char buffer[SCI_BUFSIZ+1] = {0};
+    
+    // variables to test for successful case resolution
+    //char tilt, pancam, abort, err = 0;
+    
+    // initialize timer, LCD, and SCI modules
+    timer_init();
+    
+    msleep(16);
+    LCDinit();
+    
+    SCIinit();
+    
+    EnableInterrupts;
+    
+    // print to the screen
+    LCDprintf("Hello World");
+    
+    // grab chars from SCI and echo them back
+    for(;;){
+        SCIdequeue(buffer);
+        cmdparser(buffer);
+        memset(buffer, 0, SCI_BUFSIZ+1);
+    }
+}
+
+/***********************cmdparser*******************************
+* 
+*   Purpose: Parse the command string to call the correct function. 
+*
+*   Input: char * tempcmd: input command string.
+*
+*   Output: int result: Resulting integer value.
+*
+***************************************************************/
+void cmdparser (char * buffer) {
+    char tempcmd [CMD_LEN + 1] = {0};
+    int numchars = 0;
+    static int numcmd = 0;  
+    
+    // while we do not have a valid command and we still have characters to check,
+    while ((buffer[0]) && ((numchars + 2) < SCI_BUFSIZ )) 
+        { 
+          tempcmd [0] = buffer[numchars];
+          tempcmd [1] = buffer[1 + numchars]; 
+          tempcmd [2] = buffer[2 + numchars]; 
+          tempcmd [3] = 0;  		  
+        switch (cmdconv(tempcmd)) 
+        {
+            case 0:   // If no command found, go to next character.
+            seekcmd(buffer, &numchars);
+            break;
+            
+          case PNG:   // ping
+          SCIprintf("png%d",numcmd);   // echo command confirmation with stamp.
+          numcmd++;
+          numchars+=8;
+          break;
+          
+          case ABT:  // STOP THE PRESS!
+          SCIprintf("abt%d",numcmd);
+          numcmd++;
+          numchars+=8;
+          //__asm ("STOP");
+          break;
+          
+          case TLT:  // Tilt the camera.
+          SCIprintf("tlt%d",numcmd);
+          numcmd++;
+          numchars+=8;
+          break;
+          
+          case PAN:  // Tilt the camera.
+          SCIprintf("pan%d",numcmd);
+          numcmd++;
+          numchars+=8;
+          break;  	    
+                           
+          /*case TXT:  // Print to LCD.
+          SCIprintf("txt%d",numcmd);
+          numcmd++;
+          LCDprintf("%s", buffer+3);
+          numchars+=3;
+          break;  */
+          
+        }
+    }
+}
+
+/***********************cmdconv*********************************
+* 
+*   Purpose: Convert cmd string to a usable integer value.
+*
+*   Input: char * tempcmd: input command string.
+*
+*   Output: int result: Resulting integer value.
+*
+***************************************************************/
+int cmdconv ( char * tempcmd) {
+     if (!(strcmp(tempcmd, "png")))
+        return PNG;
+     else if (!(strcmp(tempcmd, "abt")))
+        return ABT;
+     else if (!(strcmp(tempcmd, "tlt")))
+        return TLT;  
+     else if (!(strcmp(tempcmd, "pan")))
+        return PAN;   
+     else if (!(strcmp(tempcmd, "txt")))
+        return TXT;
+     else 
+        return 0;
+}
+
+/***********************seekcmd********************************
+* 
+*   Purpose: Seek through buffer until a zero is found. 
+*
+*   Input: char * buffer
+*
+***************************************************************/
+void seekcmd (unsigned char * buffer, int * numchars) {
+    while ((*buffer++ != 0) && (*numchars + 2 <= SCI_BUFSIZ) )  // Seek until first zero.      
+      (*numchars)++;   
+    while ((*buffer++ == 0) && (*numchars + 2 <= SCI_BUFSIZ) ) // Seek until non-zero is found. (beginning of new cmd).
+      (*numchars)++;
+}
---
+++ sci.c
@@ -0,0 +1,126 @@
+/* SCI module functions */
+
+#include <mc9s12c32.h>
+#include <stdio.h>      // for vsprintf()
+#include <stdarg.h>     // for va_*() functions
+#include "sci.h"
+
+
+// Global SCI Receive ring buffer
+byte SCIbuf[SCI_BUFSIZ];
+byte SCIcount;
+
+/* Initialize SCI module */
+void SCIinit(void) {
+    SET_SCI_BAUD(BAUD9600);         // Set baud rate
+    CLR_BITS(SCICR1,SCICR1_INIT);   // Set port configuration
+    
+    SCIflush();
+    SCI_RX_INT_ENABLE;  // Enable SCI receive interrupt
+    
+    SET_BITS(SCICR2,( SCICR2_RE_MASK | SCICR2_TE_MASK ));   // Enable transmitter and receiver
+    
+    SCIcount = 0;   // Clear SCI received bytes counter
+}
+
+/* Flush SCI ring buffer */
+void SCIflush(void) {
+    unsigned char i;
+    for(i=0; i < SCI_BUFSIZ; i++)
+        SCIbuf[i] = '\0';
+}
+
+/* Empty internal SCI buffer */
+void SCIdequeue(byte * ch){
+    byte bytenum;
+    static byte i = 0;
+    
+    // Dequeue only if buffer contains enough bytes for a full command
+    if (SCIcount >= SCI_CMDSIZ) {
+        // Loop through all bytes in command
+        for (bytenum=0; bytenum < SCI_CMDSIZ; bytenum++) {
+        DisableInterrupts;
+          *ch++ = SCIbuf[i];
+          SCIbuf[i] = '\0';
+          i = (i+1) % SCI_BUFSIZ;
+          SCIcount--;   // Decrement global buffer byte counter
+        EnableInterrupts;
+        }
+    }
+}
+
+/* Write char to SCI */
+void SCIputc(char c) {
+    while(!(SCISR1 & SCISR1_TDRE_MASK));    // Wait for transmit register to become empty
+    SCIDRL = c;
+}
+
+/* Write string to SCI */
+void SCIputs(const char *str) {
+    while(*str)
+        SCIputc(*str++);
+}
+
+#pragma MESSAGE DISABLE C1420   // Function call result ignored warning disable (for vsprintf)
+/* Write formatted string to SCI */
+void SCIprintf(const char *fmt, ... ) {
+    char buffer[SCI_BUFSIZ];
+    
+    va_list args;
+    va_start(args, fmt);
+    vsprintf(buffer, fmt, args);
+    va_end(args);
+    
+    SCIputs(buffer);
+}
+
+/* Read char from SCI */
+byte SCIgetc(void) {
+    byte c;
+    
+    while(!(SCISR1 & SCISR1_RDRF_MASK));    // Wait for receive register to become empty
+    c = SCIDRL;
+    
+    // Catch escape sequences
+    switch(c) {
+    case '~':   /* clear screen */
+        c = '\a';
+        break;
+    case '`':   /* move cursor to beginning */
+        c = '\r';
+        break;
+    case '\\':  /* move cursor to second line */
+        c = '\n';
+        break;
+    case '|':   /* delete one char */
+        c = '\b';
+        break;
+    default:
+        break;
+    }
+    return c;
+}
+
+/* Read string from SCI */
+/* NOTE: not working, probably not needed if using ISR for SCI receieve */
+char *SCIgets(void) {
+    char *str = NULL;
+    while(!(SCISR1 & SCISR1_IDLE_MASK))     // Get chars unitl receieve line goes idle
+        *(str++) = SCIgetc();
+    return str;
+}
+
+/*****************************************************************************/
+
+/* SCI interrupt handler */
+interrupt VectorNumber_Vsci void SCI_RX_ISR(void) {
+    static byte i = 0;
+    
+    // Check error flags before grabbing chars
+    if(!(SCISR1 & ( SCISR1_OR | SCISR1_NF | SCISR1_FE | SCISR1_PF ))) {
+        SCIbuf[i] = SCIgetc();  // Store received byte into ring buffer
+        i = (i+1) % SCI_BUFSIZ; // Increment to next index; wrap to beginning if buffer overflows
+        if(SCIcount < SCI_BUFSIZ)
+            SCIcount++;     // Increment global buffer byte counter
+    }
+}
