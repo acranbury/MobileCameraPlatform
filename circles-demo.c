4ba5c2dc06a6f15e048cc8e9e28289b62961fd9a
acranbury <acranbury-cc@conestogac.on.ca>
2013-04-25 16:49:18 -0400

    Final demo main files

---
+++ circles-demo.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "serial.h"
+
+int main(int argc, char *argv[]){
+}
---
+++ straight-lines-demo.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include "serial.h"
+
+#define SPINMODIFIER	2.356	// turns degrees into wheel distance
+
+void WaitForDTMF(int tone);
+void Drive(int distance, int time);
+void Spin(int degrees);
+
+int main(int argc, char *argv[]){
+	
+	// open and initialize the serial port
+	SerialOpen();
+	SerialInit();
+	
+	// Wait at A until hears DTMF 1
+	WaitForDTMF(1);
+	
+	// Travels a straight path for 1.73m in 10 sec
+	Drive(173, 10);
+
+	
+	// Turns on the spot 90 degrees
+	Spin(90);
+	
+	// Wait at B until hears DTMF 5
+	WaitForDTMF(5);
+	
+	// Travels a straight path for 1 m in 10 sec
+	Drive(100, 10);
+	
+	// Turns on the spot 120 degrees
+	Spin(120);
+	
+	// Wait at C until hears DTMF 9
+	WaitForDTMF(9);
+	
+	// Travels a straight path for 2 m in 10 sec
+	Drive(200, 10);
+	
+	// Turns on the spot 150 degrees and stops
+	Spin(150);
+	
+	SerialClose();
+	
+	return(0);
+}
+
+// wait for a DTMF tone
+void WaitForDTMF(int tone){
+}
+
+// drive forwards a distance in some time
+void Drive(int distance, int time){
+	char buffer[33];
+	
+	// compute the speed
+	int speed = distance/time;
+	
+	// write the distance command to the serial port
+	sprintf(buffer, "dst00%3ddst01%3ddst10%3ddst11%3d", speed, distance, speed, distance);
+	SerialWrite((unsigned char *)buffer, 32);
+}
+
+// spin the robot some degrees
+void Spin(int degrees){
+	char buffer[9];
+	
+	// compute the distance the wheels need to travel
+	float distance = degrees * SPINMODIFIER;
+	
+	// write the spin command to the serial port
+	sprintf(buffer, "spn%5d", (int)distance);
+	SerialWrite((unsigned char *)buffer, 8);
+}
